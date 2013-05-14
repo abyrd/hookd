2314fcc9daa90db42b732dbae4b7c7d44413b632
Andrew Byrd <andrew@fastmail.net>
2013-05-14 04:43:56 +0200

    daemonize, signal handling, avoid chdir

--- hook_handler.py
+++ hook_handler.py
@@ -4,9 +4,11 @@ import time, json, sys, cgi, traceback
 from BaseHTTPServer import BaseHTTPRequestHandler, HTTPServer
 import Queue, threading, syslog, subprocess, os
 
+# debian package python-daemon
+import daemon, daemon.pidlockfile, signal
+
 PORT = 8888
 BASE_DIR = '/home/abyrd/'
-DAEMON = False
 N_WORKER_THREADS = 2
 WORK_SCRIPT = ''
 LOG_IDENT = 'otphook'
@@ -33,6 +35,14 @@ class WorkerThread (threading.Thread) :
         threading.Thread.__init__(self)
         self.threadID = thread_id
         self.name = 'thread %d' % thread_id
+        self.dir = BASE_DIR + '/thread_%d_workspace' % self.threadID
+        # note: cannot change current directory per thread. multiprocessing?
+        try :
+            os.chdir (self.dir)
+        except :
+            info ('thread %s workspace directory does not exist, creating' % self.threadID)
+            os.mkdir (self.dir)
+            os.chdir (self.dir)
     def run (self) :
         global q, qlock, shutdown
         while True :
@@ -51,31 +61,24 @@ class WorkerThread (threading.Thread) :
                 qlock.release()
                 self.do_work (work_unit)
                 # print self.name, 'got', commit_sha1
-    def call (self, cmd) :
+    def call (self, cmd, cwd) :
         info ("calling '%s'" % cmd)
-        result = subprocess.call( cmd.split(), stdout=self.log, stderr=self.log )
-        info ("result of '%s' was %s" % (cmd, 'OK' if result == 0 else 'FAIL'))       
+        result = subprocess.call( cmd.split(), stdout=self.log, stderr=self.log, cwd=cwd )
+        info ("result of '%s' was %s" % (cmd, 'OK' if result == 0 else 'FAIL'))
     def do_work (self, work_unit) :
         repo_name, repo_url, head_commit = work_unit
-        dir = BASE_DIR + '/thread_%d_workspace' % self.threadID
+        repo_dir = '%s/%s' % (self.dir, repo_name)
         try :
-            os.chdir (dir)
-        except :
-            info ('thread workspace directory does not exist, creating')
-            os.mkdir (dir)
-            os.chdir (dir)
-        try :
-            os.chdir (repo_name)
+            os.chdir (repo_dir)
         except :
             info ('repository does not exist, cloning')
-            result = subprocess.call( ['git', 'clone', repo_url, repo_name], stdout=sys.stdout )
+            result = subprocess.call( ['git', 'clone', repo_url, repo_name], stdout=sys.stdout, cwd=self.dir)
             info ('result of clone operation was %d' % result)
-            os.chdir (repo_name)
-        self.log = open ('%s/build_log_%s' % (dir, head_commit), 'w')
-        self.call ('git fetch')
-        self.call ('git clean -f')
-        self.call ('git checkout ' + head_commit)
-        self.call( 'mvn clean package' )
+        self.log = open ('%s/build_log_%s' % (self.dir, head_commit), 'w')
+        self.call ('git fetch', repo_dir)
+        self.call ('git clean -f', repo_dir)
+        self.call ('git checkout ' + head_commit, repo_dir)
+        self.call( 'mvn clean package', repo_dir)
         self.log.close()
         
 # from https://github.com/abyrd/hooktest/settings/hooks
@@ -122,37 +125,59 @@ class HookHandler(BaseHTTPRequestHandler):
         log_lock.acquire()
         syslog.syslog(syslog.LOG_ERR, fmt % args)
         log_lock.release()
-        
-        
-def main():  
+
+daemonize = False
+server=None
+workerThreads = []
+def terminate() :
+    info ('terminate callback')
+    if server is not None :
+        server.socket.close()
     global q, qlock, shutdown
-    log_options = (0 if DAEMON else syslog.LOG_PERROR) | syslog.LOG_PID 
-    syslog.openlog(LOG_IDENT, logoption=log_options, facility=syslog.LOG_DAEMON)
-    workerThreads = []
+    shutdown.set() # notify worker threads that we are shutting down
+    qlock.acquire()
+    qlock.notifyAll() # wake up all threads waiting on the queue
+    qlock.release()
+    for thread in workerThreads:
+        thread.join(1)
+    info ("All worker threads exited, now exiting main thread.")
+    log_lock.acquire()
+    syslog.closelog()
+    sys.exit(0)
+
+def main() :  
+    log_options = (0 if daemonize else syslog.LOG_PERROR) | syslog.LOG_PID 
+    log_facility = syslog.LOG_DAEMON if daemonize else syslog.LOG_USER
+    syslog.openlog(LOG_IDENT, logoption=log_options, facility=log_facility)
     for thread_id in range(N_WORKER_THREADS) :
         thread = WorkerThread(thread_id)
         workerThreads.append(thread)
         thread.start()
     info ('Spawned %d worker threads.' % N_WORKER_THREADS)
     info ('Starting HTTP Github commit hook handler.')
-    server = HTTPServer(('', PORT), HookHandler)
-    try:
+    try :
+        server = HTTPServer(('', PORT), HookHandler)
         server.serve_forever()
-        #double-fork?
-    except KeyboardInterrupt:
+    except KeyboardInterrupt :
+        # SIGINT perhaps via ^C
         info ('SIGINT received, github hook handler shutting down.')
-        server.socket.close()
-        # notify worker threads that we are shutting down
-        shutdown.set()
-        qlock.acquire()
-        qlock.notifyAll() # wake up all threads waiting on the queue
-        qlock.release()
-        for thread in workerThreads:
-            thread.join(1)
-        info ("All worker threads exited, now exiting main thread.")
-        syslog.closelog()
+    except SystemExit : 
+        # SystemExit is raised by daemon library on receiving SIGTERM
+        info ('SIGTERM received, github hook handler shutting down.')
+    except Exception as e:
+        info ('Server failed: %s' % e)
+    terminate()
         
-if __name__ == '__main__':
-    main()
-
+if __name__ == '__main__' :
+    if len(sys.argv) > 1 and sys.argv[1] == '-d' :
+        daemonize = True
+    if daemonize :
+        # lockfile support seems to be screwed up
+        # does not prevent multiple copies running
+        # pidfile=daemon.pidlockfile.PIDLockFile('/var/run/githook.pid'),
+        context = daemon.DaemonContext(working_directory = '/var/githook')
+        with context :
+            main()
+    else :
+        main()
 
